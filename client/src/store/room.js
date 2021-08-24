6ae72e8c5c568413c4fd6828d6b592b206be89b1
chriscauley <chris@lablackey.com>
2021-08-24 18:55:56 +0000

    tweaking position of one room

--- client/src/store/room.js
+++ client/src/store/room.js
@@ -3,6 +3,15 @@ import downloadJson from '@/lib/downloadJson'
 
 import prepareItem from './prepareItem'
 
+const migrations = { // eslint-disable-line
+  moveEnergyRoom: (room_store) => {
+    // this room is shifted in the image. Temporarily moving it
+    const room = room_store.getOne(30)
+    room.xys = [[38 - 2/16, 38]]
+    room_store.save(room)
+  }
+}
+
 export default () => {
   const room_store = LocalStorage('room', { prepareItem })
   room_store.getAll = () => room_store.getPage({ per_page: 1e9 })?.items || []
