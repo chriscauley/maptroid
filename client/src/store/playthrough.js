6cd8f3ad3542ea5d7c8a12cc341cc13d1f3fa7fa
chriscauley <chris@lablackey.com>
2021-09-07 16:33:40 +0000

    weird bug in playthroughs was casing duplicates

--- client/src/store/playthrough.js
+++ client/src/store/playthrough.js
@@ -1,7 +1,8 @@
 import Store from './Store'
+import initial from '@/../public/sm/playthrough.json'
 
 export default () => {
-  const store = Store('playthrough')
+  const store = Store('playthrough', initial)
   store.fetchOrCreateByWorldId = (world_id) => {
     return store.fetchPage({ query: { world_id } }).then(({ items }) => {
       return items[0] || { world_id, actions: [] }
