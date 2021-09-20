2fa3c2bf35007fe7e9f13b7c144ebf2e312691e3
chriscauley <chris@lablackey.com>
2021-09-20 00:07:05 +0000

    adding some links to nav

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,8 +1,14 @@
 <template>
   <header :class="css.nav.outer()">
     <section :class="css.nav.section('left')">
-      <router-link to="/" :class="css.nav.brand()">Maptroid</router-link>
+      <router-link to="/" :class="css.nav.brand()">{{ title }}</router-link>
     </section>
+    <section class="unrest-grid" v-if="world">
+      <router-link v-for="[link, name] in world_links" :key="link" :to="link" class="link">
+        {{ name }}
+      </router-link>
+    </section>
+    <div class="flex-grow" />
     <section class="unrest-grid">
       <unrest-dropdown class="btn -light" :items="items">
         <i class="fa fa-bars" />
@@ -22,6 +28,23 @@ export default {
       { to: '/spritegallery/', text: 'Item Gallery' },
     ]
     return { css, items }
+  },
+  computed: {
+    world_links() {
+      const { world_id } = this.$route.params
+      return [
+        [`/admin/main/world/${world_id}/`, 'Admin'],
+        [`/viewer/${world_id}/`, 'Viewer'],
+        [`/editor/${world_id}/`, 'Editor'],
+      ]
+    },
+    world() {
+      const { world_id } = this.$route.params
+      return world_id && this.$store.world.getOne(world_id)
+    },
+    title() {
+      return this.world?.name || 'Project Maptroid'
+    },
   },
 }
 </script>
