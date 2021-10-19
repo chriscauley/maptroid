ad3f52fbfa1dc83e373770eacc144e0a53957384
chriscauley <chris@lablackey.com>
2021-10-19 13:08:19 +0000

    links to dread maps

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>TODO: Home</div>
+  <div>
+    <h2>Dread Editor</h2>
+    <div v-for="zone in dread_zones" :key="zone.id">
+      <router-link :to="`/dread/${zone.id}/${zone.slug}/`">
+        {{ zone.name }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   __route: {
     path: '/',
+  },
+  computed: {
+    dread_zones() {
+      return this.$store.zone.getPage({ query: { world: 3 } })?.items || []
+    },
   },
 }
 </script>
