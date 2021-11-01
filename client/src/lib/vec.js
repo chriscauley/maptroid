eb4bbcbe1e8f394b4263457e353b15254569858e
chriscauley <chris@lablackey.com>
2021-11-01 23:26:58 +0000

    more vec functions

--- client/src/lib/vec.js
+++ client/src/lib/vec.js
@@ -1,5 +1,8 @@
 const add = (xy1, xy2) => [xy1[0] + xy2[0], xy1[1] + xy2[1]]
+const subtract = (xy1, xy2) => [xy1[0] - xy2[0], xy1[1] - xy2[1]]
 const isEqual = (xy1, xy2) => xy1[0] === xy2[0] && xy1[1] === xy2[1]
+const magnitude = (xy) => Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1])
+const scale = (xy, scale) => [xy[0] * scale, xy[1] * scale]
 
 export default {
   dxyByName: {
@@ -9,5 +12,8 @@ export default {
     right: [1, 0],
   },
   add,
+  subtract,
   isEqual,
+  magnitude,
+  scale,
 }
