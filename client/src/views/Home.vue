7b8d83def7c002c66504ca5bcb311cc8eca2c4f4
chriscauley <chris@lablackey.com>
2021-11-15 15:11:56 +0000

    home now redirects to dread

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,32 +1,5 @@
 <template>
-  <div>
-    <h2>Dread</h2>
-    <table class="table">
-      <tr v-for="zone in dread_zones" :key="zone.id">
-        <td>{{ zone.id }}</td>
-        <td>
-          <router-link class="link" :to="`/dread/${zone.slug}/`">
-            {{ zone.name }}
-          </router-link>
-        </td>
-        <td v-if="$auth.user?.is_superuser">
-          <router-link class="link" :to="`/dread/${zone.slug}/?mode=room`">
-            Edit Rooms
-          </router-link>
-        </td>
-        <td v-if="$auth.user?.is_superuser">
-          <router-link class="link" :to="`/dread/${zone.slug}/?mode=screenshots`">
-            Edit Screenshots
-          </router-link>
-        </td>
-      </tr>
-    </table>
-
-    <h2>Misc</h2>
-    <div v-for="link in links" :key="link">
-      <router-link :to="link">{{ link }}</router-link>
-    </div>
-  </div>
+  <div />
 </template>
 
 <script>
@@ -34,13 +7,8 @@ export default {
   __route: {
     path: '/',
   },
-  computed: {
-    dread_zones() {
-      return this.$store.zone.getPage({ query: { per_page: 500, world: 3 } })?.items || []
-    },
-    links() {
-      return ['/screenshotupload/', '/dread/sprites/']
-    },
+  mounted() {
+    this.$router.replace('/dread/')
   },
 }
 </script>
