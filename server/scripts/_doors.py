d7379d443bbaf3bff1bb9c3751966831da65950a
chriscauley <chris@lablackey.com>
2022-03-15 12:53:22 +0000

    tweaking door test script

--- server/scripts/_doors.py
+++ server/scripts/_doors.py
@@ -1,22 +1,16 @@
 # Trying to figure out how to find doors in layer-1 and plm
 from _setup import get_world_zones_from_argv
 import cv2
+import numpy as np
 import sys
 
-from maptroid.doors import populate_room_doors, draw_doors, get_can_icons, get_halfcan_icons
+from maptroid.doors import get_door_icons
 
 
 if __name__ == "__main__":
-    world, zones = get_world_zones_from_argv()
-    can_icons = get_can_icons()
-    layer_name = 'layer-1'
-    if '-plm' in sys.argv:
-        layer_name = 'plm_enemies'
-    if '-half' in sys.argv:
-        can_icons = get_halfcan_icons()
-    for zone in zones:
-        for room in zone.room_set.all():
-            populate_room_doors(room)
-            layer1 = cv2.imread(f'.media/smile_exports/{world.slug}/{layer_name}/{room.key}')
-            result = draw_doors(layer1, room.data['doors'], cans=can_icons)
-            cv2.imwrite(f'.media/trash/{room.key}', result)
+    _icons = get_door_icons('ypx', 'can')
+    for orientation, icons_by_color in _icons.items():
+        results = []
+        for color, icon in icons_by_color.items():
+            results.append(icon)
+        cv2.imwrite(f'.media/trash/{orientation}.png', np.hstack(results))
