2e45c97635a7f0b8c4d51b54a065d6e1832f0f54
chriscauley <chris@lablackey.com>
2022-06-12 14:11:28 +0000

    show sprite in admin

--- server/sprite/admin.py
+++ server/sprite/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from sprite.models import PlmSprite, MatchedSprite
 
+class BaseSpriteAdmin(admin.ModelAdmin):
+    readonly_fields = ['_img_tag']
+    def _img_tag(self, obj=None):
+        if obj:
+            w = obj.data['width']*4
+            s = 'image-rendering: pixelated'
+            return mark_safe(f'doot <img src="{obj.url}" width="{w}" style="{s}" />')
+
+
 @admin.register(MatchedSprite)
-class MatchedSpriteAdmin(admin.ModelAdmin):
+class MatchedSpriteAdmin(BaseSpriteAdmin):
     pass
 
+
 @admin.register(PlmSprite)
-class PlmSpriteAdmin(admin.ModelAdmin):
+class PlmSpriteAdmin(BaseSpriteAdmin):
     pass
