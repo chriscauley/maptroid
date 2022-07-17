0928e9dc50ac52eee0ad37fc4e5d9ffdca9f6a2b
chriscauley <chris@lablackey.com>
2022-07-17 13:19:38 +0000

    script to rename bad room keys (eg super_metroid_XXX.png to lost-world_XXX.png)

---
+++ server/scripts/rename_smile_files.py
@@ -0,0 +1,23 @@
+from _setup import get_wzr
+import os
+
+from maptroid.models import Room
+
+world, _, _ = get_wzr()
+
+for d in ['smile_exports', 'sm_cache']:
+    ROOT = f'.media/{d}/{world.slug}'
+    dirs = ['layer-1', 'layer-2', 'bts', 'plm_enemies', 'layer-2+layer-1']
+    for _dir in dirs:
+        _dir = os.path.join(ROOT, _dir)
+        if not os.path.exists(_dir):
+            continue
+        files = [s for s in os.listdir(_dir) if s.endswith('.png') and s.startswith('super_metroid')]
+        for f in files:
+            src = os.path.join(_dir, f)
+            dest = os.path.join(_dir, f.replace('super_metroid', world.slug))
+            os.rename(src, dest)
+
+for room in world.room_set.all():
+    room.key = room.key.replace('super_metroid', world.slug)
+    room.save()
