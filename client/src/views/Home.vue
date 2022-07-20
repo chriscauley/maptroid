47d5e7b78bb48cb55e48efc1f81e5c3229d89ae8
chriscauley <chris@lablackey.com>
2022-07-20 19:34:12 +0000

    cleaning up home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="app__home">
-    <router-link
-      v-for="world in $store.route.worlds"
-      :key="world.id"
-      class="world-card"
-      :to="`/sm/${world.slug}/`"
-    >
-      <div class="world-card__map" :style="bg(world)" />
-      {{ world.slug }}
-    </router-link>
+    <h2>Projcet Maptroid</h2>
+    <div class="world-card__list">
+      <router-link
+        v-for="world in worlds"
+        :key="world.id"
+        class="world-card"
+        :to="`/sm/${world.slug}/`"
+      >
+        <div class="world-card__map" :style="bg(world)" />
+        {{ world.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { sortBy } from 'lodash'
+
 export default {
   __route: {
     path: '/',
+  },
+  computed: {
+    worlds() {
+      let { worlds } = this.$store.route
+      return sortBy(worlds.filter(w => !w.hidden), 'name')
+    }
   },
   methods: {
     bg(world) {
