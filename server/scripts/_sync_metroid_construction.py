5eac870ae2337ce1b49e58cbbc92dc11f3a74f3d
chriscauley <chris@lablackey.com>
2022-08-01 15:40:45 +0000

    updating sync script to include rating and difficulty

--- server/scripts/_sync_metroid_construction.py
+++ server/scripts/_sync_metroid_construction.py
@@ -8,13 +8,15 @@ keymap = [
     ('Author: ', 'author'),
     ('Average runtime:', 'runtime'),
     ('Genre:', 'genre'),
+    ('Rating:', 'rating'),
+    ('Difficulty:', 'difficulty'),
 ]
 
 ahist = defaultdict(int)
 ghist = defaultdict(int)
 
 for world in World.objects.filter(mc_id__isnull=False):
-    print('\n', world)
+    print(f'\n{world}')
     html = curl(f'https://metroidconstruction.com/hack.php?id={world.mc_id}')
     soup = BeautifulSoup(html, 'html.parser')
     mc_data = world.data['mc_data'] = {}
@@ -23,11 +25,17 @@ for world in World.objects.filter(mc_id__isnull=False):
         for string, key in keymap:
             if text.startswith(string):
                 value = text.split(string)[-1]
-                if key == 'genre':
+                if key == 'genre' or key == 'difficulty':
                     value = value.split('[?]')[0] # genre help text
                 if key == 'runtime':
                     value = value.split('Average')[0] # collection rate
-                mc_data[key] = value.strip()
+                if key == 'rating':
+                    text = str(div)
+                    value = text.count('/star.png') + text.count('/half_star.png')
+                if isinstance(value, str):
+                    value = value.strip()
+                print(key, value)
+                mc_data[key] = value
 
     ghist[mc_data.get('genre')] += 1
     ahist[mc_data.get('author')] += 1
