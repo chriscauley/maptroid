a5d499bdd353d01dde1d9144490f671b9acfc77d
chriscauley <chris@lablackey.com>
2022-08-05 23:00:47 +0000

    show a warning if metroid construction title does not match maptroid title

--- server/scripts/_sync_metroid_construction.py
+++ server/scripts/_sync_metroid_construction.py
@@ -14,12 +14,19 @@ keymap = [
 
 ahist = defaultdict(int)
 ghist = defaultdict(int)
+warnings = []
+
+def _clean(name):
+    return name.lower().replace(' ', '')
 
 for world in World.objects.filter(mc_id__isnull=False):
     print(f'\n{world}')
     html = curl(f'https://metroidconstruction.com/hack.php?id={world.mc_id}')
     soup = BeautifulSoup(html, 'html.parser')
     mc_data = world.data['mc_data'] = {}
+    title = soup.title.string
+    if not _clean(world.name) in _clean(title):
+        warnings.append(f'Map name might not be correct "{world}" not in "{title}"')
     for div in soup.select('.underboxD'):
         text = div.text.strip()
         for string, key in keymap:
@@ -41,3 +48,5 @@ for world in World.objects.filter(mc_id__isnull=False):
     ahist[mc_data.get('author')] += 1
     world.save()
 
+for w in warnings:
+    print(f"WARNING: {w}")
