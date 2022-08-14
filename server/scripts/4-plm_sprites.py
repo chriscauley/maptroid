6f5ed08e72311ff434c87075f623e54027274e56
chriscauley <chris@lablackey.com>
2022-08-13 20:12:55 -0400

    indent

--- server/scripts/4-plm_sprites.py
+++ server/scripts/4-plm_sprites.py
@@ -5,34 +5,34 @@ from _setup import get_wzr
 from maptroid import plm
 
 def main(rooms):
-  world, zones, rooms = get_wzr()
-  fails = []
-  hashes = []
-  matches = {}
-  match = miss = 0
+    world, zones, rooms = get_wzr()
+    fails = []
+    hashes = []
+    matches = {}
+    match = miss = 0
 
-  for room in rooms:
-    if not 'plm_enemies' in room.data:
-      fails.append(f'FAIL: missing data.plm_enemies for #{room.id} {room.key}')
-      continue
-    plms = [plm for plm in room.data['plm_enemies'] if not plm.get('deleted')]
-    if len(plms) != len(set([str(p['xy']) for p in plms])):
-      fails.append(f'FAIL: room has confusing plms: {room.name or room.id}')
-      continue
-    plm.finalize(room)
-    hashes += plm.extract_plmsprites_from_room(room)
+    for room in rooms:
+        if not 'plm_enemies' in room.data:
+            fails.append(f'FAIL: missing data.plm_enemies for #{room.id} {room.key}')
+            continue
+        plms = [plm for plm in room.data['plm_enemies'] if not plm.get('deleted')]
+        if len(plms) != len(set([str(p['xy']) for p in plms])):
+            fails.append(f'FAIL: room has confusing plms: {room.name or room.id}')
+            continue
+        plm.finalize(room)
+        hashes += plm.extract_plmsprites_from_room(room)
 
-  for hash_ in hashes:
-    if hash_ in matches:
-      match += 1
-    else:
-      matches[hash_] = True
-      miss += 1
+    for hash_ in hashes:
+        if hash_ in matches:
+            match += 1
+        else:
+            matches[hash_] = True
+            miss += 1
 
-  print(f'miss:{miss}, match:{match}')
-  for f in fails:
-    print(f)
+    print(f'miss:{miss}, match:{match}')
+    for f in fails:
+        print(f)
 
 if __name__ == '__main__':
-  _, _, rooms = get_wzr()
-  main(rooms)
+    _, _, rooms = get_wzr()
+    main(rooms)
