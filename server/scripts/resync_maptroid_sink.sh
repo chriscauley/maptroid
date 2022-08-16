2b2c2b8b6464998b8b6b7e52e72ced3c737f120d
chriscauley <chris@lablackey.com>
2022-08-16 01:35:13 +0000

    renaming file to be more descriptive

--- server/scripts/resync_maptroid_sink.sh
+++ server/scripts/resync_maptroid_sink.sh
@@ -1,6 +1,7 @@
 
 for i in bts layer-1 layer-2 plm_enemies bts-extra;
 do
+    mkdir -p .media/smile_exports/$1/$i/;
     cp ~chriscauley/projects/_maptroid-sink/$1/$i/*.png .media/smile_exports/$1/$i/;
 done
 # cp ~chriscauley/projects/_maptroid-sink/$1/plm_overrides/*.png .media/smile_exports/$1/plm_enemies/
