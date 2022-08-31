a3ca7898e67b5f8f3184451409c54fae24765139
chriscauley <chris@lablackey.com>
2022-08-30 21:02:01 -0400

    special exception to disable certain layers for grand prix maps

--- client/src/views/sm/ToolStorage.js
+++ client/src/views/sm/ToolStorage.js
@@ -1,3 +1,5 @@
+import { startCase } from 'lodash'
+
 import ToolStorage from '@/components/unrest/ToolStorage'
 import { LAYER_NAMES, INITIAL_LAYERS } from './ConfigPopper.vue'
 
@@ -37,6 +39,27 @@ export default (component) => {
 
   const initial = INITIAL_LAYERS
   const storage = ToolStorage('tools__dread', { tools: getTools, initial })
+  if (component.$route.params.world_slug.startsWith('grand-prix')) {
+    const target_settings = {
+      show_bts: false,
+      'show_layer-1': true,
+      show_rooms: false,
+      show_walls: false,
+    }
+    const changed = []
+    Object.entries(target_settings).forEach(([key, value]) => {
+      if (storage.state[key] !== value) {
+        changed.push(key)
+        storage.state[key] = value
+      }
+    })
+    if (changed.length > 0) {
+      const _changed = changed.map((s) => startCase(s.replace('show_', '')))
+      component.$ui.toast.info(
+        `To properly display this map several layers were toggled (${_changed.join(', ')})`,
+      )
+    }
+  }
   storage.getVisibleLayers = () => LAYER_NAMES.filter((n) => storage.state[`show_${n}`])
   storage.all_layers = LAYER_NAMES
 
